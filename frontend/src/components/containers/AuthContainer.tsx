1b6796132343a9b35ff484c01cdadb11b4e9652d
omar <o.wakeel@intouch.com>
2024-01-28 16:59:02 +0200

    featL add sign in initial page

---
+++ frontend/src/components/containers/AuthContainer.tsx
@@ -0,0 +1,26 @@
+import React, { PropsWithChildren } from 'react'
+import { Link } from 'react-router-dom';
+
+interface Props extends PropsWithChildren {
+    title: string;
+}
+
+const AuthContainer: React.FC<Props> = (props) => {
+    const { title, children } = props;
+
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 pt-48 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h1 className='text-center'>
+                    <Link to="/">
+                        Easy Generators
+                    </Link>
+                </h1>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{title}</h2>
+            </div>
+            {children}
+        </div>
+    )
+}
+
+export default AuthContainer;
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,6 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { ISignInForm } from "../utilities/interfaces/auth.interface";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import signInSchema from "../utilities/schemas/sign-in";
+import AuthContainer from "../components/containers/AuthContainer";
+
 const SignIn = () => {
+
+    const methods = useForm<ISignInForm>({
+        mode: "onChange",
+        criteriaMode: "all",
+        shouldFocusError: true,
+        reValidateMode: "onChange",
+        resolver: yupResolver(signInSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    })
+
     return (
-        <div>SignIn</div>
+        <AuthContainer title="Sign in to your account">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <FormProvider {...methods} >
+                    <form className="space-y-6" >
+
+                    </form>
+                </FormProvider>
+            </div>
+        </AuthContainer>
     )
 }
 
