6ddc6ba02093c65dc443ff1281801d03f96dc6a1
omar <o.wakeel@intouch.com>
2024-01-28 17:03:51 +0200

    feat: add layout components

---
+++ frontend/src/components/containers/PageContainer.tsx
@@ -0,0 +1,33 @@
+import React, { PropsWithChildren } from 'react'
+import Loader from '../layout/Loader';
+import Error from '../layout/Error';
+
+interface Props extends PropsWithChildren {
+    title: string;
+    isLoading?: boolean;
+    error?: Error | null;
+}
+
+const PageContainer: React.FC<Props> = ({ title, isLoading = false, error = null, children }) => {
+
+    if (isLoading) {
+        return (
+            <Loader />
+        )
+    }
+
+    if (error) {
+        return (
+            <Error message={error.message} />
+        )
+    }
+
+    return (
+        <div className='flex flex-col items-center my-4' >
+            <h1 className='text-center' > {title} </h1>
+            {children}
+        </div>
+    )
+}
+
+export default PageContainer
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,9 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import { ISignInForm } from "../utilities/interfaces/auth.interface";
 import { FormProvider, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import signInSchema from "../utilities/schemas/sign-in";
 import AuthContainer from "../components/containers/AuthContainer";
+import PageContainer from "../components/containers/PageContainer";
 
 const SignIn = () => {
 
@@ -20,15 +20,17 @@ const SignIn = () => {
     })
 
     return (
-        <AuthContainer title="Sign in to your account">
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <FormProvider {...methods} >
-                    <form className="space-y-6" >
+        <PageContainer title="">
+            <AuthContainer title="Sign in to your account">
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <FormProvider {...methods} >
+                        <form className="space-y-6" >
 
-                    </form>
-                </FormProvider>
-            </div>
-        </AuthContainer>
+                        </form>
+                    </FormProvider>
+                </div>
+            </AuthContainer>
+        </PageContainer>
     )
 }
 
