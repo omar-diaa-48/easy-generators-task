826abffd369f7d708f9c6b6b50bd0c190d1b3263
omar <o.wakeel@intouch.com>
2024-01-28 17:08:21 +0200

    feat: add sign in form

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,9 +4,10 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import signInSchema from "../utilities/schemas/sign-in";
 import AuthContainer from "../components/containers/AuthContainer";
 import PageContainer from "../components/containers/PageContainer";
+import TextFieldInput from "../components/handlers/TextFieldInput";
+import Button from "../components/handlers/Button";
 
 const SignIn = () => {
-
     const methods = useForm<ISignInForm>({
         mode: "onChange",
         criteriaMode: "all",
@@ -19,13 +20,23 @@ const SignIn = () => {
         }
     })
 
+    const { handleSubmit, formState } = methods;
+
+    const handleUserSubmit = (form: ISignInForm) => {
+        console.log(form)
+    }
+
     return (
         <PageContainer title="">
             <AuthContainer title="Sign in to your account">
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <FormProvider {...methods} >
-                        <form className="space-y-6" >
-
+                        <form className="space-y-6" onSubmit={handleSubmit(handleUserSubmit)}>
+                            <TextFieldInput name="email" label="Email" variant="outlined" />
+                            <TextFieldInput name="password" label="Password" type="password" variant="outlined" />
+                            <div>
+                                <Button disabled={!formState.isValid} type="submit">Sign In</Button>
+                            </div>
                         </form>
                     </FormProvider>
                 </div>
